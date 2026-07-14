d5070f18bfab1b015e54c390ee8c04f64f399534
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-07-14 23:09:39 +0530

    Update day01.py

--- day01.py
+++ day01.py
@@ -4,4 +4,7 @@ print("Hello", "World!", sep=" ", end=" ")
 a=int(input("Enter Your Image: "))
 
 
-#
+#print statements:
+
+# sep used to seperate, end used to end statements , multiple statements in one print statements
+#input Function i.e. a=input("....") stores value as string by default 
