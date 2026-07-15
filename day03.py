fb6befeef4ab3f55555cc01c28d1ac9d5f94daef
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-07-16 02:50:08 +0530

    Update day03.py

--- day03.py
+++ day03.py
@@ -8,3 +8,8 @@ print(set(Price))
 
 a={ 1, 2, 3,4,5,6,7,8}
 
+# adding , removing , converting data type etc are the functions that are performed in this 
+#same operation are carried out by tuple 
+#as well dictionary 
+#practicing them again and again makes them remember 
+#q
