71831763f3a7a22205aa4e99c2ef7549209be092
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-07-17 22:58:21 +0530

    Update day06.py

--- day06.py
+++ day06.py
@@ -3,3 +3,14 @@
 # //= gives you integer quotient
 # %= gives you remainder
 # a**b = a to the power b
+a=int(input())
+b=int(input())
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+these are all 
