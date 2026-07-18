3786a909989910b67af1ee4882f45861891bb697
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-07-18 21:21:06 +0530

    Update day07.py

--- day07.py
+++ day07.py
@@ -1,1 +1,12 @@
-# function :
+# function :instead of writing a long code again and again we just go with it and write a block of code giving it a name 
+#lines of code gets less , code becomes more efficient 
+
+#simple function 
+def sum():
+  print("Sum")
+
+#argumented function :
+
+def sum(int a , int b):
+  sum=a+b
+  print(sum)
